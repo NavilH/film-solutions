3acd4b2954a937496a7b864c6afcffa8228d41c4
Navil Hernandez <navilhrnndz@gmail.com>
2026-03-07 21:30:10 -0500

    Defined API_BASE

--- client/src/pages/Liked.js
+++ client/src/pages/Liked.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
+import { API_BASE } from "../config";
 const Liked = () => {
     const [liked, setLiked] = useState([]);
     //const userId = 1; 
